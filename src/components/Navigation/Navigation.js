c9a3c78392e9b8d67a70cf2031e071bf784d7b17
Yuanzheng Li <yuanzhengliwork@gmail.com>
2019-01-30 08:32:44 -0800

    Add Profile Icon component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ProfileIcon from '../Profile/ProfileIcon';
 
 const Navigation = ({onRouteChange, isSignedIn}) => {
     if(isSignedIn) {
       return (
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+          <ProfileIcon />
           <p className="f3 link dim black pa3 pointer" onClick={() => onRouteChange('signout')}>Sign Out</p>
         </nav>
       );
---
+++ src/components/Profile/ProfileIcon.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class ProfileIcon extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  render() {
+    return (
+      <div class="pa4 tc">
+        <img
+          src="http://tachyons.io/img/logo.jpg"
+          class="br-100 h3 w3 dib" alt="avatar" />
+      </div>
+    );
+  }
+}
+
+export default ProfileIcon;
